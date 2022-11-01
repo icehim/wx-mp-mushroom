596e721ecda4057458984ab3413b88eb596399df
zhiming <zhiming.hao@qq.com>
2022-11-01 20:07:54 +0800

    编程式导航跳转详情页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swipers:[]
+    swipers:[], // 轮播图
+    courses:[] // 推荐课程
   },
 
   /**
@@ -13,6 +14,7 @@ Page({
    */
   onLoad(options) {
     this.getSwipersData()
+    this.getCoursesData()
   },
   async getSwipersData(){
     const {status,message} = await wx.$request({
@@ -23,5 +25,32 @@ Page({
         swipers:message
       })
     }
+  },
+  async getCoursesData(){
+    const {status,message} = await wx.$request({
+      url:'home/course'
+    })
+    if(status === 0) {
+      this.setData({
+        courses:message
+      })
+    }
+  },
+  // tab切换到课程也买你
+  switchToCourse(){
+    // switchTab=>不会销毁当前页面栈，只会打开新的页面栈
+    wx.switchTab({
+      url: '/pages/course/course',
+    })
+  },
+  goToDetail(e){
+    // e.target.dataset 代表拿到的是事件触发源那个节点上的自定义属性
+    // e.currentTarget.dataset 代表拿到的是事件处理源节点上的自定义属性
+    const id = e.currentTarget.dataset.id
+    // 编程式跳转
+    wx.navigateTo({
+      url: '/subpkg/course-detail/course-detail?id='+id,
+    })
   }
+
 })
