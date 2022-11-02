4f0046ed0447a9d4898ec3bb3407bf74333c64bc
zhiming <zhiming.hao@qq.com>
2022-11-02 20:25:01 +0800

    我的页面渲染完成

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,62 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getuserInfoData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  
+  async getuserInfoData(){
+    const {status,message} = await wx.$request({
+      url:'my/info'
+    })
+    if(status === 0){
+      this.setData({
+        userInfo:message
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  contact(){
+    wx.makePhoneCall({
+      phoneNumber: '404',
+    })
   }
 })
